b067ae424eae2a13d760568d02946c4104e3a6d5
Jason Nathan <jjnathanjr@gmail.com>
2016-06-04 11:54:19 +0800

    A simple server side method to retrieve a user by a given email address

--- imports/api/server/account-methods.js
+++ imports/api/server/account-methods.js
@@ -1,3 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { _ } from 'lodash';
+import Emailer from '/imports/startup/server/email.js';
+
+
+Meteor.methods({
+
+    /**
+     * A simple method that finds a user by his email or throws an error 
+     * if one is not found
+     * 
+     * @param  {String} email A valid email address
+     * @return {String}       The user id of the found user
+     */
+    'accounts.findByEmail': function(email) {
+        check(email, String);
+        let user = Accounts.findUserByEmail(email);
+
+        if (!user) {
+            throw new Meteor.Error(403, "No record found for that email address")
+        }
+
+        return user._id;
+    }
+});
