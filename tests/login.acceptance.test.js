90516654f2c4dd2e2e21b754ca1aab0eca3aa9a4
Jason Nathan <jjnathanjr@gmail.com>
2016-06-04 10:56:39 +0800

    Added some basic placeholder tests for or r login and registration

---
+++ tests/login.acceptance.test.js
@@ -0,0 +1,5 @@
+describe("As an user, I would like to sign into the system", function() {
+    it("should allow me to sign in using my email and password");
+    it("should allow me to reset my password if I had forgotten mine");
+    it("should allow me to see a secret message after I have successfully logged in");
+});
---
+++ tests/registration.acceptance.test.js
@@ -0,0 +1,6 @@
+describe("As an user, I would like to sign up for an account", function() {
+    it("should prompt me with an error if the email I used is already in use within the system");
+    it("should prompt me with an error if my passwords don't match");
+    it("should send me an email with a link that allows me to verify my account and log in to the system");
+    it("should allow me to use an SMS verification token to verify my account instead");
+});
