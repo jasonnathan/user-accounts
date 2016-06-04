3e26f4d2262f9685b8b210c1df5d1c7d3a3bf936
Jason Nathan <jjnathanjr@gmail.com>
2016-06-04 13:00:48 +0800

    Added routing and a default template

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,67 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+
+
+//import templates
+import '/imports/ui/layouts/default.js';
+
+/**
+ * Setting up password reset trigger here, because the route should automatically go to the form if a token
+ * was detected
+ */
+Session.setDefault('_resetPasswordToken', Accounts._resetPasswordToken);
+Session.setDefault('_enrollAccountToken', Accounts._enrollAccountToken);
+
+const publicRoutes = FlowRouter.group({});
+
+
+publicRoutes.route('/', {
+    name: 'home',
+    triggersEnter: [function(context, redirect) {
+    	// the default template handles redirects if a user is not found
+        redirect('/dashboard');
+    }]
+});
+
+publicRoutes.route('/signIn', {
+    name: 'signIn',
+    action() {
+        BlazeLayout.render("default", { authTemplate: null, pubTemplate: "loginForm" });
+    }
+});
+
+publicRoutes.route('/forgot-password', {
+    name: 'forgetPassword',
+    action() {
+        BlazeLayout.render("default", { authTemplate: null, pubTemplate: "forgetPasswordForm" });
+    }
+});
+
+publicRoutes.route('/reset-password/:token', {
+    name: 'resetPassword',
+    action() {
+        BlazeLayout.render("default", { authTemplate: null, pubTemplate: "resetpasswordForm" });
+    }
+});
+
+publicRoutes.route('/signup/:token', {
+    name: 'register',
+    action() {
+        BlazeLayout.render("default", { authTemplate: null, pubTemplate: "signupForm" });
+    }
+});
+
+FlowRouter.route('/dashboard', {
+    name: 'dashboardHome',
+    action() {
+        BlazeLayout.render("default", { authTemplate: "welcome", pubTemplate: null });
+    }
+});
+
+FlowRouter.notFound = {
+    action() {
+        BlazeLayout.render("centeredLayout", { main: "t404" });
+    }
+}
